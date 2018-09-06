b489532c8b97b898039d4eeccdde8725b6cc2159
Sylvain Comte <sylvain.comte@yahoo.fr>
2018-09-06 15:02:05 +0200

    transformed geolocation macro to widget
    commit for the record before deleting the buggy old geolocation macro

--- plugins/sycom/leaflet/ext/geolocation-macro.js
+++ plugins/sycom/leaflet/ext/geolocation-macro.js
@@ -5,11 +5,10 @@ module-type: macro
 
 \*/
 (function () {
-
     /*jslint node: true, browser: true */
     /*global $tw: false */
     "use strict";
-    
+console.log(this);
     exports.name = "geoLoc";
     exports.params = [
         { name: "text" },
@@ -20,16 +19,18 @@ module-type: macro
         ]; // some not used for now. Will enable tiddler destination + point / line / polygon choice + accuracy (+geo format - json ...?)
     
     exports.run = function(text, tiddler, icon, accuracy, type) {
-        if (!tiddler) {
-            var tiddler = this.getVariable("currentTiddler");
-        }
+        var this_tiddler = this.getVariable("currentTiddler");
+        tiddler = tiddler || this_tiddler;
+        // if tiddler parameter is not given, current tiddler is the target
+//        if (tiddler == "" || tiddler == null || tiddler === undefined) tiddler = this_tiddler;
 		/* create the button */
         var buttonContent;
 		// if text parameter, will use it for the button
         if (text) {
             buttonContent = text;
         } else {
-		// if not, looking for an icon
+        // if not, looking for an icon
+        /* !todo: ?question: should the icon depend on "type" parameter? */
             buttonContent = $tw.wiki.getTiddler(icon).fields.text;
         }
         var geoButton = document.createElement("button");
@@ -37,31 +38,44 @@ module-type: macro
 		geoButton.param = {"tiddler": tiddler};
         geoButton.addEventListener("click", function() {
 			getLocation(tiddler, accuracy, type) 				
-			}, false);
-        this.parentDomNode.append(geoButton);
+            }, false);
+        //if(this.parentDomNode.childNodes) this.parentDomNode(geoButton);
     }
     
     function getLocation(tiddler, accuracy, type) {
             if (navigator.geolocation) {
-                navigator.geolocation.getCurrentPosition(function(position) {
-					setPosition(position, tiddler, accuracy, type);
-				});
-            } else { 
-                /* !todo: ?question: replace by "modal" alert? */
-				$tw.utils.error("geolocation access denied or not supported by browser :" + error);     
+                navigator.geolocation.getCurrentPosition(
+                    // success callback
+                    function(position) {
+                        setPosition(position, tiddler, accuracy, type);
+                    },
+                    // error callback
+                    function(error) {
+                        noLocation(error.message + ". You may not are connected via httpS://")
+                    } 
+				);
+            } else {
+				noLocation("position disabled or not supported by your browser");     
         }
+    }
+
+    function noLocation(message) {
+        /* !todo: ?question: replace by "modal" alert? */
+        // create or update a temporary message tiddler 
+        $tw.wiki.setText("$:/temp/noLocationMessage","text",null,"geolocation access denied: " + message,null);
+        // displays it in modal
+        $tw.modal.display("$:/temp/noLocationMessage");
     }
     
     function setPosition(position, tiddler, accuracy, type) {
     /* to replace by field populating (point(s) polyline(s) or polygon(s)) */
 	/* default : if tiddler has point field, will create a points field to store new data;
 		if tiddler has points field, will add a new point to the tiddler field;	*/
-console.log( "Latitude: " + position.coords.latitude + "Longitude: " + position.coords.longitude);
+console.log( "Lat: " + position.coords.latitude + " - Long: " + position.coords.longitude);
 		var newPoint = position.coords.latitude + "," + position.coords.longitude;
 	/* check if tiddler has a geofield */
 		var track = "";
 		var flds = $tw.wiki.getTiddler(tiddler).fields
-console.log(flds);
 		if(flds.points) {
 			track = flds.points;
 			$tw.wiki.setText(tiddler,"points",null,track + " " + newPoint,null);
@@ -72,7 +86,8 @@ console.log(flds);
 				$tw.wiki.setText(tiddler,"points",null,track + " " + newPoint,null);
 				$tw.wiki.setText(tiddler,"point",null,null,null);
 			} else $tw.wiki.setText(tiddler,"point",null,newPoint,null);	
-		}
+        }
+        /* !todo: tells the user the point "x,y" has been added to "type" in "tiddler" */
 	}
     
     })();
